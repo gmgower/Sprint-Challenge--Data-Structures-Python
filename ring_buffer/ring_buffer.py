1e0b587f01003a81d6a9551f01ec8aa102e618e8
@gmgower <gmgower@gmail.com>
2020-05-31 22:22:37 -0500

    Create append and get func.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,18 +4,50 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current_node = None # a, b, c, d
+        self.current = None # a, b, c, d
         self.storage = DoublyLinkedList()
 
     def append(self, item):
         # check size of the storage is less than the capacity
         if len(self.storage) < self.capacity:
-            # add to tail previous
+            #add item to the tail
             self.storage.add_to_tail(item)
-            # set current node variable to the head 
-            self.current_node = self.storage.head
-        # check if the capacity as been reached
+            return
 
 
     def get(self):
-        pass
+        # create a blank array 
+        buffer = []
+        # declare a current variable & set it to the head of the
+        current = self.storage.head
+        # while there still a current value
+        while current:
+            # append the value to the buffer
+            buffer.append(current.value)
+            # move the current reference to the next node in the
+            current = current.next
+        return buffer
+
+"""
+    ```python
+buffer = RingBuffer(3)
+
+buffer.get()   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+buffer.get()   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+
+buffer.get()   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+buffer.get()   # should return ['d', 'e', 'f']
+```
+"""
